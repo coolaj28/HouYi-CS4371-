1cc3a2496adbf1baf079c12fd2239f6d20729fd5
eps60 <eps60@txstate.edu>
2025-04-15 22:35:19 -0500

    openai_util.py

--- util/openai_util.py
+++ util/openai_util.py
@@ -1,11 +1,17 @@
-import openai
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
 
+load_dotenv()
 
-def completion_with_chatgpt(text: str, model: str = "gpt-4") -> str:
-    response = openai.ChatCompletion.create(
+openai_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=openai_key)
+
+def completion_with_chatgpt(text: str, model: str = "gpt-3.5-turbo") -> str:
+    response = client.chat.completions.create(
         model=model,
         messages=[
             {"role": "user", "content": text},
         ],
     )
-    return response["choices"][0]["message"]["content"]
+    return response.choices[0].message.content
